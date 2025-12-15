ace43b214479dd56e9f6886a86ee4c22aaa0b8de
Haseeb Shaukat <haseebshaukat12@gmail.com>
2025-12-15 21:52:41 +0500

    page fix

--- study-app-frontend/src/components/pages/LandingPage.tsx
+++ study-app-frontend/src/components/pages/LandingPage.tsx
@@ -1,24 +1,21 @@
 // app/page.tsx
-import Navbar from "@/components/pages/Navbar"
+'use client';
 
+import Navbar from "@/components/pages/Navbar"
 import Header from "@/components/pages/Header"
 import CardSection from "@/components/pages/CardSection"
 import MockTests from "@/components/pages/MockTests"
 import Instructors from "@/components/pages/Instructors"
-
-
 import Testimonials from "@/components/pages/Testimonials"
 import Footer from "@/components/pages/Footer"
-
 
 export default function LandingPage() {
   return (
     <>
-    <Navbar/>
+      <Navbar/>
       <Header />
       <CardSection />
       <MockTests/>
-
       <Testimonials />
       <Instructors/>
       <Footer/>
